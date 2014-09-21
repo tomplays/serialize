11a229b506dbae7bc51f46808dd28dbc59339804
tomplays <homeof@gmail.com>
2014-09-21 23:38:43 +0200

    img bug / title h1-h2 support

--- src/inline.js
+++ src/inline.js
@@ -15,13 +15,31 @@ exports.serializeInline = function (elem) {
 
   if (name === 'a')
     serializations.push({ type: Types.link, href: elem.getAttribute('href') })
+  
+
+  // BUG 1
+  else if (name === 'omg')
+    serializations.push({ type: Types.omg, src: elem.getAttribute('src') })
+  else if (name === 'img')
+    serializations.push({ type: Types.omg, src: elem.getAttribute('src') })
+  
+
+
   else if (name === 'code')
     serializations.push({ type: Types.code })
+  
+  else if (name === 'h1')
+    serializations.push({ type: Types.title, level: 1})
+
+  else if (name === 'h2')
+    serializations.push({ type: Types.title, level: 2})
+
+
   else if (/^(strong|b)$/.test(name))
     serializations.push({ type: Types.bold })
   else if (/^(em|i)$/.test(name))
     serializations.push({ type: Types.italic })
-
+ 
   if (/italic|oblique/.test(elem.style.fontStyle))
     serializations.push({ type: Types.italic })
   if (elem.style.fontWeight === 'bold' || elem.style.fontWeight >= 700)
@@ -43,6 +61,13 @@ exports.createInline = function (markup) {
     case Types.link:
       markupElem = 'a'
       break
+    // BUG /2
+    case Types.omg:
+      markupElem = 'omg'
+      break
+    case Types.img:
+      markupElem = 'img'
+      break
     case Types.code:
       markupElem = 'code'
       break
@@ -52,7 +77,15 @@ exports.createInline = function (markup) {
     case Types.italic:
       markupElem = 'em'
       break
-
+    case Types.title:
+      if( markup.level === 1){
+          markupElem = 'h1'
+      }
+      if(markup.level === 2){
+          markupElem = 'h2'
+      }
+    
+      break
     default:
       throw new Error('Unknown markup type')
   }
@@ -63,5 +96,10 @@ exports.createInline = function (markup) {
   if (markup.type === Types.link)
     markupElem.setAttribute('href', markup.href)
 
+  // bug 3
+  if (markup.type === Types.omg)
+    markupElem.setAttribute('src', markup.src)
+  if (markup.type === Types.img)
+    markupElem.setAttribute('src', markup.src)
   return markupElem
 }
--- src/types.js
+++ src/types.js
@@ -1,10 +1,13 @@
 'use strict';
 
 var Types = {
-  link: 1,
-  code: 2,
-  bold: 3,
-  italic: 4
+  link: 2,
+  code: 3,
+  bold: 4,
+  italic: 5,
+  title: 6,
+  img:7,
+  omg:8
 }
 
 module.exports = Types
